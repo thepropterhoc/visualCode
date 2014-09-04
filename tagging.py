7a5466dfbb103ed23285f82526dcb4b3a8e691f1
Shelby <vanhooser@ou.edu>
2014-09-03 22:18:29 -0500

    FUCKING HELL IT WORKS

--- tagging.py
+++ tagging.py
@@ -1,6 +1,6 @@
-import ctags
+import ctags, sys
 from ctags import CTags, TagEntry
-import sys
+import pygraphviz as pgv
 
 try:
 	tagFile = CTags('tags')
@@ -8,20 +8,36 @@ except:
 	sys.exit(1)
 
 entry = TagEntry() 
+
+patterns = {}
+files = {}
 # TAG_PARTIALMATCH - begin with
 # TAG_FULLMATCH - full length matching
 # TAG_IGNORECASE - disable binary search
 # TAG_OBSERVECASE - case sensitive and allowed binary search to perform
 
-if tagFile.find(entry, 'BeautifulSoup', ctags.TAG_PARTIALMATCH | ctags.TAG_IGNORECASE):
-    print 'found'
-    print entry['lineNumber']
-    print entry['pattern']
-    print entry['kind']
-
 # Find the next tag matching the name and options supplied to the 
 # most recent call to tagFile.find().  (replace the entry if found)
-status = tagFile.findNext(entry)
-
-# Step to the next tag in the file (replace entry if found)
 status = tagFile.next(entry)
+while status:
+	fileName, pattern = entry['file'], entry['name']
+	if fileName not in files:
+		files[fileName] = [pattern]
+	else:
+		files[fileName] += [pattern]
+	if pattern not in patterns:
+		patterns[pattern] = [fileName]
+	else:
+		patterns[pattern] += [fileName]
+	status = tagFile.next(entry)
+
+
+
+patternGraph = pgv.AGraph()
+
+for pattern in patterns.keys():
+	for fileName in patterns[pattern]:
+		patternGraph.add_edge(pattern, fileName)
+
+patternGraph.layout()
+patternGraph.draw('output.png')
